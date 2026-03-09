f9fe7c87f7b58529de8c791ad2354a9f61c8f545
kgilliam18-csn <kenneth.gilliam@student.csn.edu>
2026-03-09 16:13:08 -0600

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,3 +1,5 @@
 import streamlit as st
-with st.chat_message("user"):
-  st.write("Hello! My name is user")
+import numpy as np
+with st.chat_message("assistant"):
+  st.write("Hello human")
+  st.bar_chart(np.random.randn(30, 3))
