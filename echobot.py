34afe5f301d9657ae1de777f4b24a3e5626b065d
kgilliam18-csn <kenneth.gilliam@student.csn.edu>
2026-03-09 16:19:05 -0600

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,4 +1,8 @@
 import streamlit as st
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+
 prompt = st.chat_input("Say something")
 if prompt:
   st.write(f"User has sent the following prompt: {prompt}")
